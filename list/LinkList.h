daf128fb41441da3b8dd3cdc0784aac84821392a
wangleif <wangleif@jiedaibao.com>
2019-08-03 14:51:17 +0800

    add

---
+++ list/LinkList.h
@@ -0,0 +1,10 @@
+#ifndef LIST_LINKLIST_H
+#define LIST_LINKLIST_H
+typedef int ElemeType;
+typedef struct Node
+{
+    ElemeType data;
+    struct  Node *next;
+};
+
+#endif
---
+++ list/header.h
@@ -0,0 +1,8 @@
+#ifndef LIST_HEADER
+#define LIST_HEADER
+#define NULL 0;
+#define TRUE 1;
+#define FALSE 0;
+#define OK 1;
+#define ERROR 0;
+#endif
